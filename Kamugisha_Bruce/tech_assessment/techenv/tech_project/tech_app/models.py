5ebdfa1f43dd7c05f3912c1d6fd6d43bca6d652c
Kamugisha Bruce <brucekamugisha36@gmail.com>
2023-04-28 13:28:50 +0300

    TECH ASSESSMENT

---
+++ Kamugisha_Bruce/tech_assessment/techenv/tech_project/tech_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Registration(models.Model):
+    firstname = models.CharField(max_length=100,null=True,blank=False)
+    lastname = models.CharField(max_length=100,null=True,blank=False)
+    date_of_birth = models.DateField(max_length=100,null=True,blank=False)
+    gender = models.CharField(max_length=100,null=True,blank=False)
+    country = models.CharField(max_length=100,null=True,blank=False)
+    state_district = models.CharField(max_length=100,null=True,blank=False)
+    town = models.CharField(max_length=100,null=True,blank=False)
+    zipcode = models.CharField(max_length=100,null=True,blank=False)
+    phone1 = models.IntegerField(default=0, null=True,blank=False)
+    phone2 = models.IntegerField(default=0, null=True,blank=False)
+    email = models.EmailField(max_length=1000,null=True,blank=False)
+    def __str__(self):
+        return self.firstname
